5a9927a9709ebda956a6d877ad5e885d2c5572c9
Al Wold <alwold@alwold.com>
2014-10-11 14:11:16 -0700

    Initial Commit

---
+++ GarageDoorOpener/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GarageDoorOpener
+//
+//  Created by Al Wold on 10/11/14.
+//  Copyright (c) 2014 Al Wold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GarageDoorOpener/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GarageDoorOpener
+//
+//  Created by Al Wold on 10/11/14.
+//  Copyright (c) 2014 Al Wold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
